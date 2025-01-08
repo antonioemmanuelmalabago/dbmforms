f952b7b3f0b0e884a83b9582597df756477159ab
Moriarty <aecmalabago2020@plm.edu.ph>
2025-01-08 22:09:09 +0800

    Update

--- client/src/utils/getInitials.js
+++
@@ -1,6 +0,0 @@
-export const getInitials = (fullname) => {
-  const names = fullname.split(' ')
-  const initials = names.slice(0, 2).map((name) => name[0].toUpperCase())
-  const initialsStr = initials.join('')
-  return initialsStr
-}
