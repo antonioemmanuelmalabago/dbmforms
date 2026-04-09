8b20ae14f27a7e21ded524d456cfbee0eab40658
Antonio Emmanuel Malabago <malabagoantonioemmanuel@gmail.com>
2026-04-10 01:58:25 +0800

    Fixed "Cannot read properties of undefined (reading 'split')"

--- client/src/utils/getInitials.js
+++ client/src/utils/getInitials.js
@@ -1,4 +1,5 @@
 export const getInitials = (fullname) => {
+  if (!fullname) return ''
   const names = fullname.split(' ')
   const initials = names.slice(0, 2).map((name) => name[0].toUpperCase())
   const initialsStr = initials.join('')
