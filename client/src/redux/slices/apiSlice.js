4ba294d0f0ecb2dd389fc58fa27d616d8e7c3e96
Antonio Emmanuel Malabago <malabagoantonioemmanuel@gmail.com>
2026-04-10 02:13:30 +0800

    Reverted previous changes. Changed base url in apiSlice.js file.

--- client/src/redux/slices/apiSlice.js
+++ client/src/redux/slices/apiSlice.js
@@ -1,6 +1,8 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
-const baseQuery = fetchBaseQuery({ baseUrl: '' })
+const baseQuery = fetchBaseQuery({
+  baseUrl: 'https://dbmforms-backend.onrender.com',
+})
 
 export const apiSlice = createApi({
   baseQuery,
