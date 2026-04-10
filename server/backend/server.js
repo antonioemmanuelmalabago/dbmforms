9509bd2a77a7443e78a09d2b17f4870c03ad0eaf
Antonio Emmanuel Malabago <malabagoantonioemmanuel@gmail.com>
2026-04-10 18:45:28 +0800

    Fixed static files cannot be found issue.

--- server/backend/server.js
+++ server/backend/server.js
@@ -10,6 +10,9 @@ import formRoutes from './routes/formRoutes.js'
 import path from 'path'
 import { fileURLToPath } from 'url'
 
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 connectDB()
 
 const app = express()
@@ -22,14 +25,12 @@ app.use(cookieParser())
 app.use('/api/users', userRoutes)
 app.use('/api/forms', formRoutes)
 
-app.use(notFound)
-app.use(errorHandler)
-
-app.listen(port, () => console.log(`Server started on port ${port}`))
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 app.use(express.static(path.join(__dirname, '../../client/dist')))
 app.get('*', (req, res) =>
   res.sendFile(path.join(__dirname, '../../client/dist/index.html')),
 )
+
+app.use(notFound)
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
